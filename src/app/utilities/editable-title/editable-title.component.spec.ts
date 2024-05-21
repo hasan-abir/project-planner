1044e15111e4f84c7a89da97dcf005f35b3679c9
hasan-abir <contact.hasanabir@gmail.com>
2024-05-21 09:06:43 +0600

    Editable title tests

--- src/app/utilities/editable-title/editable-title.component.spec.ts
+++ src/app/utilities/editable-title/editable-title.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditableTitleComponent } from './editable-title.component';
+import { By } from '@angular/platform-browser';
 
 describe('EditableTitleComponent', () => {
   let component: EditableTitleComponent;
@@ -18,5 +19,35 @@ describe('EditableTitleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should render the title with the textContent', () => {
+    const textContent = 'Example';
+    component.textContent = textContent;
+    fixture.detectChanges();
+
+    const title = fixture.nativeElement.querySelector('h2');
+    expect(title.textContent).toEqual(` ${textContent} `);
+  });
+  it('should render the textbox', () => {
+    component.textboxOpen = true;
+    fixture.detectChanges();
+
+    const textbox = fixture.nativeElement.querySelector('input');
+
+    expect(textbox).toBeTruthy();
+  });
+  it('should change the title into textbox', () => {
+    const title = fixture.nativeElement.querySelector('h2');
+
+    title.click();
+    expect(component.textboxOpen).toBeTrue();
+  });
+  it('should change the textbox into title', () => {
+    component.textboxOpen = true;
+    fixture.detectChanges();
+    const textbox = fixture.debugElement.query(By.css('input'));
+
+    textbox.triggerEventHandler('focusout', null);
+    expect(component.textboxOpen).toBeFalse();
   });
 });
