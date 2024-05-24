a25b68c83b08c23222388119e63f12272d19040d
hasan-abir <contact.hasanabir@gmail.com>
2024-05-24 08:52:18 +0600

    Toggleable add task

--- src/app/plan/plan.component.ts
+++ src/app/plan/plan.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { CardComponent } from '../utilities/card/card.component';
 import { EditableTitleComponent } from '../utilities/editable-title/editable-title.component';
 import { CtaButtonComponent } from '../utilities/cta-button/cta-button.component';
 import { PillComponent } from '../utilities/pill/pill.component';
+import { CommonModule } from '@angular/common';
+import gsap from 'gsap';
 
 interface Label {
   id: string;
@@ -14,6 +16,7 @@ interface Label {
   selector: 'app-plan',
   standalone: true,
   imports: [
+    CommonModule,
     CardComponent,
     EditableTitleComponent,
     CtaButtonComponent,
@@ -23,8 +26,10 @@ interface Label {
   styleUrl: './plan.component.css',
 })
 export class PlanComponent {
+  @ViewChild('formRef') formRef?: ElementRef;
   @Input() headerTitle: string = '';
 
+  addTaskOpened: boolean = false;
   selectedLabels: string[] = [];
   labels: Label[] = [
     {
@@ -54,6 +59,25 @@ export class PlanComponent {
       this.selectedLabels = this.selectedLabels.filter((item) => item !== id);
     } else {
       this.selectedLabels = [id, ...this.selectedLabels];
+    }
+  }
+
+  toggleAddTask() {
+    const defaultDuration = 0.25;
+    if (this.formRef && this.addTaskOpened) {
+      gsap.to(this.formRef.nativeElement, {
+        height: 0,
+        marginTop: 0,
+        duration: defaultDuration,
+      });
+      this.addTaskOpened = false;
+    } else if (this.formRef && !this.addTaskOpened) {
+      gsap.to(this.formRef.nativeElement, {
+        height: 'auto',
+        marginTop: '0.75rem',
+        duration: defaultDuration,
+      });
+      this.addTaskOpened = true;
     }
   }
 }
